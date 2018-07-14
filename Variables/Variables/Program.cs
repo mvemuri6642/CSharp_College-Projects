93249ac2114847b01e76cadd386b30b28d23c8b4
Manohar <saimanoharvemuri333@gmail.com>
2018-07-14 20:03:41 +0530

    Add files via upload

---
+++ Variables/Variables/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            int x;
+            int y;
+
+            x = 3;
+            y = x + 5;
+            Console.WriteLine(y);
+            Console.ReadLine();
+            */
+
+
+
+            // Reading the Given Input Values:
+            Console.WriteLine("What's Your Name:");
+            Console.Write("Enter Your First Name:");
+            string myFirstName;
+            myFirstName = Console.ReadLine();
+
+            /*Console.Write("Enter Your Last Name:");
+            string myLastName;
+            myLastName = Console.ReadLine();*/
+            Console.WriteLine("Enter Your Last Name");
+            string myLastName = Console.ReadLine();
+
+            Console.WriteLine("Hello,\n" + myFirstName + " " + myLastName);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ WhileIteration/WhileIteration/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace WhileIteration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            bool displayMenu = true;
+            while (displayMenu)
+            {
+                displayMenu = MainMenu();
+            }
+        }
+        private static bool MainMenu()
+        {
+            Console.WriteLine("Choose a Number:");
+            Console.WriteLine("1.Printing Numbers:");
+            Console.WriteLine("2.Guessing Game:");
+            Console.WriteLine("3.Exit");
+            string result = Console.ReadLine();
+            if(result == "1")
+            {
+                PrintingNumbers();
+                return true;
+            }
+            else if(result == "2")
+            {
+                GuessingGame();
+                return true;
+            }
+            else if(result == "3")
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        private static void PrintingNumbers()
+        {
+            Console.Clear();
+            Console.WriteLine("Enter a Number:");
+            int result = int.Parse(Console.ReadLine());
+            int i = 0;
+            while (i < result+1)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+            Console.ReadLine();
+        }
+
+        public static void GuessingGame()
+        {
+            Console.Clear();
+            Random myRandom = new Random();
+            int randomNumber = myRandom.Next(1, 11);
+            int guesses = 0;
+            bool incorrect = true;
+
+            do
+            {
+                Console.WriteLine("Guess a Number between 1 and 10:");
+                string result = Console.ReadLine();
+                guesses++;
+                if (result == randomNumber.ToString())
+                    incorrect = false;
+                else
+                    Console.WriteLine("Wrong!");
+
+            } while (incorrect);
+            Console.WriteLine("Correct it took you:{0} guesses", guesses);
+            Console.ReadLine();
+        }
+    }
+}
